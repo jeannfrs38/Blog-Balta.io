766d3d1339994658ad0bea7e5fa33243ab7a3746
jeannfrs <jeanfrs38@gmail.com>
2025-02-17 08:13:13 -0300

    finalizando CRUD Tag

--- Screens/TagScreens/DeleteTagScreen.cs
+++ Screens/TagScreens/DeleteTagScreen.cs
@@ -1,9 +1,38 @@
+using Blog.Models;
+using Blog.Repositories;
+
 namespace Blog.Screens.TagScreens;
 
 public class DeleteTagScreen
 {
     public static void Load()
     {
-        
+        Console.Clear();
+        Console.WriteLine("Deletar Tag");
+        Console.WriteLine("--------------");
+        Console.WriteLine();
+        Console.Write("Qual o id da Tag que deseja excluir? ");
+        var id = int.Parse(Console.ReadLine());
+
+        Deletar(id);
+
+        Console.ReadKey();
+        MenuTagScreen.Load();
+
+    }
+
+    public static void Deletar(int id)
+    {
+        try
+        {
+            var repository = new Repository<Tag>(Database.Connection);
+            repository.Delete(id);
+            Console.WriteLine("Tag excluida com sucesso");
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine("Nao foi possivel excluir a tag");
+            Console.WriteLine(ex.Message);
+        }
     }
 }
--- Screens/TagScreens/UpdateTagScreen.cs
+++ Screens/TagScreens/UpdateTagScreen.cs
@@ -1,9 +1,51 @@
+using Blog.Models;
+using Blog.Repositories;
+
 namespace Blog.Screens.TagScreens;
 
 public class UpdateTagScreen
 {
     public static void Load()
     {
+        Console.Clear();
+        Console.WriteLine("Atualizar tag");
+        Console.WriteLine("-----------");
+        Console.WriteLine();
+        Console.Write("Qual o Id da Tag: ");
+        var id = int.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+        Console.Write("Novo nome: ");
+        var name = Console.ReadLine();
+        Console.Write("Novo slug: ");
+        var slug =  Console.ReadLine();
+        Update(new Tag()
+        {
+            Id = id,
+            Name = name,
+            Slug = slug
+        });
+
+        Console.ReadKey();
+        MenuTagScreen.Load();
+
+
+
+    }
+
+    public static void Update(Tag tag)
+    {
+        try
+        {
+            var repository = new Repository<Tag>(Database.Connection);
+            repository.Update(tag);
+            Console.WriteLine("Tag atualizada com sucesso");
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine("Nao foi possivel Atualizar a Tag");
+            Console.WriteLine(ex.Message);
+        }
         
     }
 }
